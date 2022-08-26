ee02f06fab79455893a172deafb7aec8e0b8b771
HCDCU <83141499+HCDCU@users.noreply.github.com>
2022-08-25 23:57:58 -0500

    Add files via upload

---
+++ es_cr.dict.jb-networkdevices.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @copyright   Copyright (C) 2021 Jeffrey Bostoen
+ * @license     https://www.gnu.org/licenses/gpl-3.0.en.html
+ * @version     2021-08-26 16:05:02
+ *
+ * Localized data
+ */
+
+Dict::Add('ES CR', 'Spanish', 'Español, Castellano', array(
+	'Class:Firewall' => 'Firewall',
+	'Class:MiscNetworkDevice' => 'Misc. Network Device',
+	'Class:Router' => 'Router',
+	'Class:WirelessAccessPoint' => 'Wireless Access Point',
+	'Class:NetworkSwitchCore' => 'Switch Core',
+	'Class:NetworkSwitchDistribucion' => 'Switch Distribución',
+	'Class:NetworkSwitchAcceso' => 'Switch Acceso',
+));
+
+
+
